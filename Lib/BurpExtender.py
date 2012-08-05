6a5d23ead75b3800339a4549a5299c1e25efffa8
Joseph Tartaro <droogie1xp@gmail.com>
2012-08-05 16:04:32 -0700

    Initial Framework Commit

--- Lib/BurpExtender.py
+++ Lib/BurpExtender.py
@@ -1,3 +1,23 @@
+################################################################################
+# Copyright (C) 2011-2012 Joseph Tartaro <droogie[at]burpextensions[dot]com>
+# Copyright (C) 2011-2012 James Lester <infodel[at]burpextensions[dot]com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.  You may not use, modify or
+# distribute this program under any other version of the GNU General
+# Public License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+################################################################################
+
 from burp import IBurpExtender #Burp Extension Imports
 from burp import IHttpRequestResponse
 from burp import IBurpExtenderCallbacks
